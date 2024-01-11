1bfc402473cccd1ddb3af36f82e2a7ba77df9d76
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2024-01-11 21:08:55 +0530

    Adding basic documentation structure

---
+++ script/custom.js
@@ -0,0 +1,32 @@
+const root = () => {
+    const rootPath = 'http://localhost/openorg';
+    const loadHeader = () => {
+        $('#header').load(rootPath + '/common/header.html', () => {
+            $('#logo').attr("src", rootPath + '/images/openorglogo.png');
+        });
+    };
+    const loadFooter = () => {
+        $('#footer').load(rootPath + '/common/footer.html');
+    };
+    const loadProjectHeader = (projectName) => {
+        $('#page-header').load(rootPath + '/' + projectName + '/projectHeader.html');
+    };
+    const loadProjecSideBar = (projectName) => {
+        $('#left-bar').load(rootPath + '/' + projectName + '/projectSidebar.html');
+    };
+    const loadContentText = (projectName, filename) => {
+        $('#content-text').empty();
+        $('#content-text').load(rootPath + '/' + projectName + '/pagecontent/' + filename + '.html', () => {
+            reloadPrismJs();
+        });
+    };
+    const reloadPrismJs = () => {
+        $('#prism-js').remove();
+        var script = document.createElement('script');
+        script.id = 'prism-js';
+        script.src = '../script/prism.js';
+        script.type = 'text/javascript';
+        document.getElementsByTagName('html')[0].appendChild(script);
+    };
+    return { loadHeader, loadFooter, loadProjectHeader, loadProjecSideBar, loadContentText };
+};
