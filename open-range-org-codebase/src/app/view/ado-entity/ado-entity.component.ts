7dd74f4a625375096db13d7fc503c82d216d6156
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2024-07-04 00:16:29 +0530

    Updating code highlight

--- open-range-org-codebase/src/app/view/ado-entity/ado-entity.component.ts
+++ open-range-org-codebase/src/app/view/ado-entity/ado-entity.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-ado-entity',
   templateUrl: './ado-entity.component.html',
   styleUrls: ['./ado-entity.component.css']
 })
-export class AdoEntityComponent {
-
+export class AdoEntityComponent implements AfterViewInit{
+  ngAfterViewInit() {
+    (window as any).Prism.highlightAll();
+  }
 }
--- open-range-org-codebase/src/app/view/csv-grape/csv-grape.component.ts
+++ open-range-org-codebase/src/app/view/csv-grape/csv-grape.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-csv-grape',
   templateUrl: './csv-grape.component.html',
   styleUrls: ['./csv-grape.component.css']
 })
-export class CsvGrapeComponent {
-
+export class CsvGrapeComponent implements AfterViewInit{
+  ngAfterViewInit() {
+    (window as any).Prism.highlightAll();
+  }
 }
