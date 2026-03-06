fd9e8e6b3eeaf21f21f1e9fdea88d5b263f9ea0a
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2026-03-07 01:35:12 +0530

    Adding new deployment for new extension : youngmonk

---
+++ open-range-org-codebase/src/app/view/test-case-generator/test-case-generator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-case-generator',
+  imports: [],
+  templateUrl: './test-case-generator.component.html',
+  styleUrl: './test-case-generator.component.css'
+})
+export class TestCaseGeneratorComponent {
+
+}
